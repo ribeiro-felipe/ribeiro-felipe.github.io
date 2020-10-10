9846f725c1209082dab654e9c86c9df51bebe85e
ribeiro-felipe <49889245+ribeiro-felipe@users.noreply.github.com>
2020-10-10 10:56:43 -0600

    update

--- lesson04/js/script.js
+++ lesson04/js/script.js
@@ -2,3 +2,6 @@ function toggleMenu () {
     // console.log(document.getElementById("primaryNav").classList);
     document.getElementById("primaryNav").classList.toggle("hide")
 }
+
+document.getElementById("year").innerHTML = (new Date().getFullYear())
+document.getElementById("updated").innerHTML = document.lastModified;
