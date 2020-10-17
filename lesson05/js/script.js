a5846ec5d3ec2a88e7a63b9e7640bb7e901741e0
ribeiro-felipe <49889245+ribeiro-felipe@users.noreply.github.com>
2020-10-17 16:17:22 -0600

    update

--- lesson05/js/script.js
+++ lesson05/js/script.js
@@ -3,9 +3,6 @@
     document.getElementById("primaryNav").classList.toggle("hide")
  }
 
-// document.getElementById("year").innerHTML = (new Date().getFullYear())
-// document.getElementById("updated").innerHTML = document.lastModified;
-
 
 const dow = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
