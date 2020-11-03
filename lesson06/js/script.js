0fa78306bad63a34d97c34447e5f482c6be8c259
ribeiro-felipe <49889245+ribeiro-felipe@users.noreply.github.com>
2020-11-03 09:32:03 -0700

    update

--- lesson06/js/script.js
+++ lesson06/js/script.js
@@ -11,3 +11,24 @@ let date = new Date();
 let output = dow[date.getDay()] + ', ' + date.getDate() + ' ' + month[date.getMonth()] + ' ' + date.getFullYear();
 
 document.getElementById('current-date').innerText = output;
+
+/* Compute the wind chill based on high temperature and wind speed */
+(() => {
+   const temp = document.getElementById('high-temperature').innerText;
+   const speed = document.getElementById('wind-speed').innerText;
+   const windChillElement = document.getElementById('wind-chill');
+
+   let windChill = 'n/a';
+   if (temp <= 50 && speed > 3) {
+      windChill = 35.74 + 0.6215 * temp - 35.75 * speed ** 0.16 + 0.4275 * temp * speed ** 0.16;
+      windChill = windChill.toFixed(0);
+   }
+
+   windChillElement.innerText = windChill;
+})();
+
+WebFont.load({
+   google: {
+     families: ['Merriweather Sans']
+   }
+ });
