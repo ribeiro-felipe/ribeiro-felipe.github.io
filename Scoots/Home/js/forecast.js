5d1f1807233a7b73e0978c5fa2b89d61b80daaf9
ribeiro-felipe <49889245+ribeiro-felipe@users.noreply.github.com>
2020-12-15 16:11:52 -0700

    update

--- Scoots/Home/js/forecast.js
+++ Scoots/Home/js/forecast.js
@@ -1,25 +1,34 @@
-// Forecast
+const apikey  = '329d11b9c25e61d61a5971a86a937b6a';
+const baseURL = 'http://api.openweathermap.org/data/2.5/';
+let cityid    = '3530103';
+let units     = 'imperial';
 
-let baseUrl = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&id=";
-// let cityId ="3530103";
-// const apiKey ="329d11b9c25e61d61a5971a86a937b6a";
+method = 'forecast';
+apiURL = baseURL +
+         method + '?' +
+         'id=' + cityid +
+         '&APPID=' + apikey +
+         '&units=' + units;
 
-let apiUrl = url +
-                  cityId +
-                    "&appid=" + apiKey; +
-                    
-console.log(apiUrl);
+const DOW = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+fetch(apiURL)
+   .then(response => response.json())
+   .then(data => {
+      console.log('Forecast data:');
+      console.log(data);
 
-fetch(requestUrl)
-  .then(response => response.json())
-  .then(data => {
+      let forecast_temps = document.querySelectorAll('.forecast-day .temp');
+      let forecast_dow = document.querySelectorAll('.forecast-day .dow');
+      let forecast_day_num = 0;
+      let list = data.list;
 
-    let temp =  Math.round(data.main.temp)  + ' °F';
-    document.getElementById('temperature').innerText = temp;
-    let feels_like =  Math.round(data.main.feels_like)  + ' °F';
-    document.getElementById('feelslike').innerText = feels_like;
-    let description = (data.weather[0].main);
-    document.getElementById('description').innerText = description;
-    let humidity = (data.main.humidity) + '%';
-    document.getElementById('humidity').innerText = humidity;
-  });
+
+      for (item of list) {
+         if (item.dt_txt.includes('18:00:00')) {
+            let date = new Date(item.dt * 1000);
+            forecast_dow[forecast_day_num].innerHTML = DOW[date.getDay()];
+            forecast_temps[forecast_day_num].innerHTML = Math.round(item.main.temp) + "&deg;F";
+            forecast_day_num++;
+         }
+      }
+   });
