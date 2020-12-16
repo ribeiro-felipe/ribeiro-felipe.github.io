8084459aec41087c8169880379146d65b7a9a856
ribeiro-felipe <49889245+ribeiro-felipe@users.noreply.github.com>
2020-12-16 12:15:36 -0700

    update

--- lesson10/lesson10-preston/js/weather-summary.js
+++ lesson10/lesson10-preston/js/weather-summary.js
@@ -1,36 +1,34 @@
-( () => {
+// Weather Summary
 
-   // Get current weather information
-   const apikey = 'f5389a0574c4c42c4b119e7c147d1402';
-   const baseURL = '//api.openweathermap.org/data/2.5/';
-   let cityid = '5605242';  // rexburg. Preston is 5604473
-   let method = 'weather';
-   let units = 'imperial';
-   let apiURL = baseURL +
-                method + '?' +
-                'id=' + cityid + 
-                '&APPID=' + apikey + 
-                '&units=' + units;
-   
-   // let apiURL = 'weather-api.json'; // for testing
-   fetch(apiURL)
-      .then( response => response.json())
-      .then( data => {
-         const temp = data.main.temp_max;
-         const speed = data.wind.speed;
-         const desc = data.weather[0].description;
-         const humidity = data.main.humidity;
-         document.getElementById('weather-summary-currently').innerText = desc;
-         document.getElementById('high-temperature').innerHTML = temp.toFixed(0) + ' &deg;F';
-         document.getElementById('weather-summary-humidity').innerText = humidity + '%';
-         document.getElementById('wind-speed').innerText = speed + ' mph';
-      
-         let windChill = 'n/a';
-         if (temp <= 50 && speed > 3) {
-            windChill = 35.74 + 0.6215 * temp - 35.75 * speed ** 0.16 + 0.4275 * temp * speed ** 0.16;
-            windChill = windChill.toFixed(0);
-            windChill += ' &deg;F';
-         }
-      
-         document.getElementById('wind-chill').innerHTML = windChill;
-      });
+let url = "https://api.openweathermap.org/data/2.5/weather?units=imperial&id=";
+let cityId ="5604473";
+const apiKey ="329d11b9c25e61d61a5971a86a937b6a";
+
+let requestUrl = url +
+                  cityId +
+                    "&appid=" + apiKey; +
+
+
+console.log(requestUrl);
+fetch(requestUrl)
+  .then(response => response.json())
+  .then(data => {
+
+   const description = (data.weather[0].main);
+   document.getElementById('currently').innerText = description;
+   const temp = data.main.temp_max;
+   document.getElementById('high').innerHTML = temp.toFixed(0) + ' &deg;F';
+   const humidity = (data.main.humidity) + '%';
+   document.getElementById('humidity').innerText = humidity;
+   const speed = (data.wind.speed) + ' mph';
+   document.getElementById('speed').innerText = speed;
+
+    let windChill = 'n/a';
+    if (temp <= 50 && speed > 3) {
+       windChill = 35.74 + 0.6215 * temp - 35.75 * speed ** 0.16 + 0.4275 * temp * speed ** 0.16;
+       windChill = windChill.toFixed(0);
+       windChill += ' &deg;F';
+    }
+ 
+    document.getElementById('chill').innerHTML = windChill;
+  });
